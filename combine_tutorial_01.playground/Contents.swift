e05e8aff6a684b1601c21a0eb100b708600e7de3
kimminjun9 <kimminjun9@worksmobile.com>
2023-01-03 11:43:36 +0900

    [feat] Combine & NotificationCenter와 함께 활용

--- combine_tutorial_01.playground/Contents.swift
+++ combine_tutorial_01.playground/Contents.swift
@@ -15,3 +15,34 @@ myIntArrayPublisher.sink(receiveCompletion: { completion in
     print("값을 받았다. : \(receivedValue)")
 })
 
+
+// NotificationCenter
+var mySubscription: AnyCancellable? // 하나의 Subscription
+
+var mySubscriptionSet = Set<AnyCancellable>() // 여러 개의 Subscription을 Set으로 모아 관리
+
+var myNotification = Notification.Name("com.solbat.customNotification")
+
+var myDefaultPublisher: NotificationCenter.Publisher = NotificationCenter.default.publisher(for: myNotification) // Notification과 연결된 Publisher 생성
+
+NotificationCenter.default.post(Notification(name: myNotification)) // NotificationCenter에서 post하더라도 연결된 Publisher에 대해 아직 구독을 하지 않았으므로 아무 일도 발생하지 않음
+
+mySubscription = myDefaultPublisher.sink(receiveCompletion: { completion in
+    switch completion {
+    case .finished:
+        print("완료")
+    case .failure(let error):
+        print("실패 : error : \(error)")
+    }
+}, receiveValue: { receivedValue in
+    print("받은 값 : \(receivedValue)")
+})
+
+mySubscription?.store(in: &mySubscriptionSet)
+
+NotificationCenter.default.post(Notification(name: myNotification))
+NotificationCenter.default.post(Notification(name: myNotification))
+NotificationCenter.default.post(Notification(name: myNotification))
+
+//mySubscription?.cancel()
+
