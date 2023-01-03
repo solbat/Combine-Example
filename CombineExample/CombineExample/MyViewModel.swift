0c6ae2b13e78f74649f172861e51d889351023a1
kimminjun9 <kimminjun9@worksmobile.com>
2023-01-03 15:22:59 +0900

    [feat] viewModel Input 프로퍼티 생성

--- CombineExample/CombineExample/MyViewModel.swift
+++ CombineExample/CombineExample/MyViewModel.swift
@@ -10,4 +10,15 @@ import Combine
 
 class MyViewModel {
     
+    // Published 어노테이션을 통해 구독이 가능하도록 설정
+    @Published var passwordInput: String = "" {
+        didSet {
+            print("MyViewModel / passwordInput: \(passwordInput)")
+        }
+    }
+    @Published var passwordConfirmInput: String = "" {
+        didSet {
+            print("MyViewModel / passwordConfirmInput: \(passwordConfirmInput)")
+        }
+    }
 }
